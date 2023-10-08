0e8698c2ae4488f3d4978614a74d54afcf018f52
BaFon77 <antocha-spartak@yandex.ru>
2023-10-08 21:53:21 +0300

    Add gateway and service

---
+++ shop/src/main/java/com/example/shop/ShopApplication.java
@@ -0,0 +1,15 @@
+package com.example.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
+
+}
